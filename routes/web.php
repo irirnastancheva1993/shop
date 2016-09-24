4739332a25fd341130bf358593ea8c2fd051aacc
Ирина Станчева < >
2016-09-24 23:36:00 +0300

    4 commit

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,20 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::get('/', 'MainController@indexAction');
-Route::get('/aboutus', 'MainController@aboutusAction');
-Route::get('/main', 'MainController@getMain');
-Route::get('/users', 'MainController@getAllUsers');
-Route::get('/users/{id}', 'MainController@getUser')-> where ('id', '[0-9]');
-Route::get('/goods', 'MainController@getAllGood');
-Route::get('/goods/{id}', 'MainController@getGood')-> where ('id', '[0-9]');
-Route::get('/orders', 'MainController@getAllOrders');
-Route::get('/orders/{id}', 'MainController@getOrder')-> where ('id', '[0-9]');
-Route::get('/categories', 'MainController@getAllCategories');
-Route::get('/categories/{id}', 'MainController@getCategories')-> where ('id', '[0-9]');
+Route::get('/about', function(){
+    return view('pages.about');
+});
+
+//
+//Route::get('/aboutus', 'MainController@aboutusAction');
+//Route::get('/main', 'MainController@getMain');
+//Route::get('/users', 'MainController@getAllUsers');
+//Route::get('/users/{id}', 'MainController@getUser')-> where ('id', '[0-9]');
+//Route::get('/goods', 'MainController@getAllGood');
+//Route::get('/goods/{id}', 'MainController@getGood')-> where ('id', '[0-9]');
+//Route::get('/orders', 'MainController@getAllOrders');
+//Route::get('/orders/{id}', 'MainController@getOrder')-> where ('id', '[0-9]');
+//Route::get('/categories', 'MainController@getAllCategories');
+//Route::get('/categories/{id}', 'MainController@getCategories')-> where ('id', '[0-9]');
 
 
